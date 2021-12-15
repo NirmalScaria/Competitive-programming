fa40a474862e9aed91500f7b2422ac862489cf46
NirmalScaria <alreadynirmalised@gmail.com>
2021-12-16 02:44:19 +0530

    Codeforces START20B

--- Codechef/START20B/Weird_Values.cpp
+++ Codechef/START20B/Weird_Values.cpp
@@ -12,7 +12,32 @@ using namespace std;
 #define vvll vector<vector<long long>>
 #define pb push_back
 void solve(){
-    
+  int n;
+  cin>>n;
+  vector<int> a(n);
+  for(auto &i : a) {cin >>i;}
+  map<int,vi> d;
+  for(int i=0; i<n; i++){
+      if(d.count(a[i]))
+          d[a[i]].pb(i);
+      else
+          d[a[i]]={-1,i};
+  }
+    int result = 0;
+    map<int,vi>::iterator it;
+    for(it = d.begin(); it!=d.end(); it++){
+        int val = it->first;
+        d[val].pb(n);
+        int l=1, r=val;
+        while(r<d[val].size()-1){
+            int scnt = (d[val][l] - d[val][l-1]) * (d[val][r+1]-d[val][r]) * val;
+            result += scnt;
+            l+=1;
+            r+=1;
+        }
+    }
+    cout<<result<<endl;
+    return;
 }
 int main() {
     ios_base::sync_with_stdio(0);
