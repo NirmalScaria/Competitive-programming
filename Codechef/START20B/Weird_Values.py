47b0220685adfcad7332c5fa2f082026b030e869
NirmalScaria <alreadynirmalised@gmail.com>
2021-12-15 23:38:13 +0530

    Codechef START20B

--- Codechef/START20B/Weird_Values.py
+++ Codechef/START20B/Weird_Values.py
@@ -1,28 +1,21 @@
 t=int(input())
 for _ in range(t):
     n=int(input())
-    a = list(map(int,input().split()))
-    freq=[]
-    d={}
-    for i in range(len(a)):
-        if(a[i] in d):
-            d[a[i]]+=[i]
+    arr = list(map(int,input().split()))
+    occurences={}
+    for i in range(len(arr)):
+        if(arr[i] in occurences):
+            occurences[arr[i]]+=[i]
         else:
-            d[a[i]]=[-1,i]
-    result=0
-    for item in d:
-        if(len(d[item])<item):
-            continue
-        d[item]+=[n]
-        l=1
-        r=item
-        # print('checking ',d[item],' for val',item)
-        while(r<len(d[item])-1):
-            # print('l , r are ',l,r)
-            ans = (d[item][l] - d[item][l-1]) * (d[item][r+1] - d[item][r]) * item
-            result+=ans
-            # print('added ',ans)
-            r+=1
-            l+=1
-            
-    print(result)
+            occurences[arr[i]]=[-1,i]
+    subsequenceCount=0
+    for uniqueValue in occurences:
+        occurences[uniqueValue]+=[n]
+        leftPointer=1
+        rightPointer=uniqueValue
+        while(rightPointer<len(occurences[uniqueValue])-1):
+            subSequenceCount = (occurences[uniqueValue][leftPointer] - occurences[uniqueValue][leftPointer-1]) * (occurences[uniqueValue][rightPointer+1] - occurences[uniqueValue][rightPointer]) * uniqueValue
+            subsequenceCount+=subSequenceCount
+            rightPointer+=1
+            leftPointer+=1
+    print(subsequenceCount)
